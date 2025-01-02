changed attention masking and fixed theory on lambda term of loss weighting

edm2/attention_masking.py
@@ -3,31 +3,41 @@ import warnings
 import torch
 from torch.nn.attention.flex_attention import flex_attention, create_block_mask, BlockMask, _DEFAULT_SPARSE_BLOCK_SIZE
 
-def make_train_mask(batch_size, num_heads, n_frames, image_size):
-    # image size is the number of pixels (height x width)
 
-    # I have to define this function here because it depends from image_size
-    # TODO: check if this can be turned into an external class
-    def autoregressive_diffusion_mask(b, h, q_idx, kv_idx):
-        q_idx, kv_idx = q_idx // image_size, kv_idx // image_size
+class AutoregressiveDiffusionMask:
+
+    def __init__(self, n_frames, image_size):
+        self.n_frames = n_frames
+        self.image_size = image_size
+
+
+    def __call__(self, b, h, q_idx, kv_idx):
+        q_idx, kv_idx = q_idx // self.image_size, kv_idx // self.image_size
         
         causal_mask_clean = q_idx >= kv_idx
-        causal_mask_noisy = q_idx - n_frames > kv_idx 
-        domain_attention_towards_clean = kv_idx < n_frames
-        mask_towards_clean = (causal_mask_clean ^ causal_mask_noisy ^ (q_idx < n_frames)) & domain_attention_towards_clean
+        causal_mask_noisy = q_idx - self.n_frames > kv_idx 
+        domain_attention_towards_clean = kv_idx < self.n_frames
+        mask_towards_clean = (causal_mask_clean ^ causal_mask_noisy ^ (q_idx < self.n_frames)) & domain_attention_towards_clean
 
-        self_mask_noisy = (kv_idx >= n_frames) & (q_idx == kv_idx)
+        self_mask_noisy = (kv_idx >= self.n_frames) & (q_idx == kv_idx)
         return mask_towards_clean ^ self_mask_noisy ^ domain_attention_towards_clean
 
+
+def make_train_mask(batch_size, num_heads, n_frames, image_size):
+    # image size is the number of pixels (height x width)
+    autoregressive_diffusion_mask = AutoregressiveDiffusionMask(n_frames, image_size)
 
     if n_frames*image_size*2<_DEFAULT_SPARSE_BLOCK_SIZE:
         warnings.warn(f"The masking matrix must be at least the size of the default block size,\ngot {n_frames*image_size*2} and the default block size is {_DEFAULT_SPARSE_BLOCK_SIZE}\n returning None")
         return None
     if image_size<_DEFAULT_SPARSE_BLOCK_SIZE:
         if n_frames*image_size%_DEFAULT_SPARSE_BLOCK_SIZE!=0:
-            sequence_length = n_frames*image_size*2
-            warnings.warn(f"\nThe image size must be a divisor of the default block size ({_DEFAULT_SPARSE_BLOCK_SIZE}), got image_size:{image_size} and n_frames:{n_frames}\n using {(sequence_length**2 * batch_size * num_heads)/1e6}M of memory")
-            return create_block_mask(autoregressive_diffusion_mask, B=batch_size, H=num_heads, Q_LEN=sequence_length, KV_LEN=sequence_length) 
+            warnings.warn("The image size must be a divisor of the default block size ({_DEFAULT_SPARSE_BLOCK_SIZE}), got image_size:{image_size} and n_frames:{n_frames}\n returning None")
+            return None
+
+            # sequence_length = n_frames*image_size*2
+            # warnings.warn(f"\nThe image size must be a divisor of the default block size ({_DEFAULT_SPARSE_BLOCK_SIZE}), got image_size:{image_size} and n_frames:{n_frames}\n using {(sequence_length**2 * batch_size * num_heads)/1e6}M of memory")
+            # return create_block_mask(autoregressive_diffusion_mask, B=batch_size, H=num_heads, Q_LEN=sequence_length, KV_LEN=sequence_length) 
         n_frames = n_frames*image_size//_DEFAULT_SPARSE_BLOCK_SIZE
         image_size = _DEFAULT_SPARSE_BLOCK_SIZE
     
@@ -46,13 +56,18 @@ def make_train_mask(batch_size, num_heads, n_frames, image_size):
     return BlockMask.from_kv_blocks(num_blocks_in_row, col_indices, BLOCK_SIZE=image_size, mask_mod=autoregressive_diffusion_mask)
 
 
+class DiagonalDiffusionMask:
+    def __init__(self, image_size):
+        self.image_size = image_size
+
+    def __call__(self, b, h, q_idx, kv_idx):
+        q_idx, kv_idx = q_idx // self.image_size, kv_idx // self.image_size
+        return q_idx >= kv_idx
+
 def make_infer_mask(batch_size, num_heads, n_frames, image_size):
     # image size is the number of pixels (height x width)
 
-    def diagonal_diffusion_mask(b, h, q_idx, kv_idx):
-        q_idx, kv_idx = q_idx // image_size, kv_idx // image_size
-        
-        return q_idx >= kv_idx
+    diagonal_diffusion_mask = DiagonalDiffusionMask(image_size)
 
     if n_frames*image_size<_DEFAULT_SPARSE_BLOCK_SIZE:
         warnings.warn(f"The masking matrix must be at least the size of the default block size,\ngot {n_frames*image_size} and the default block size is {_DEFAULT_SPARSE_BLOCK_SIZE}\n returning None")

edm2/loss.py
@@ -39,21 +39,24 @@ class EDM2Loss:
 
     def __call__(self, net, images, labels=None, use_loss_weight=False):
         batch_size, n_frames, channels, height, width = images.shape    
-        images = torch.cat((images,images),dim=1)
+        cat_images = torch.cat((images,images),dim=1).clone()
 
         sigma_targets = (torch.randn(batch_size,n_frames,device=images.device) * self.P_std + self.P_mean).exp()
         sigma_context = torch.rand(batch_size,1,device=images.device).expand(-1,n_frames).clone()*self.context_noise_reduction # reducing significantly the noise of the context tokens
         sigma = torch.cat((sigma_context,sigma_targets),dim=1)
 
-        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
-        noise = einops.einsum(sigma, torch.randn_like(images), 'b t, b t ... -> b t ...') 
-        denoised = net(images + noise, sigma, labels)
-        losses = ((denoised[:,n_frames:] - images[:,n_frames:]) ** 2).mean(dim=(-1,-2,-3))
-        losses = losses * weight[:,n_frames:]
+        noise = einops.einsum(sigma, torch.randn_like(cat_images), 'b t, b t ... -> b t ...') 
+        denoised = net(cat_images + noise, sigma, labels)[:,n_frames:]
+        losses = ((denoised - images) ** 2).mean(dim=(-1,-2,-3))
+
+        sigma = sigma[:,n_frames:]
+        weight = 0.5*(sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
+        losses = losses * weight
+
         if self.noise_weight is not None:
-            self.noise_weight.add_data(sigma[:,n_frames:], losses)
+            self.noise_weight.add_data(sigma, losses)
             if use_loss_weight:
-                mean_loss = self.noise_weight.calculate_mean_loss(sigma[:,n_frames:])
+                mean_loss = self.noise_weight.calculate_mean_loss(sigma)
                 losses = losses / mean_loss**2 + 2*torch.log(mean_loss)
         return losses.mean()
 #----------------------------------------------------------------------------

edm2/loss_weight.py
@@ -122,7 +122,7 @@ class MultiNoiseLoss:
         num_points = 200  # Number of data points along the sigma axis
 
         # Generate logarithmically spaced sigma values
-        sigma_values = np.logspace(-2.3, 2.7, num_points)
+        sigma_values = np.logspace(-2., 2., num_points)
 
         # --- Plotting ---
         plt.close()

edm2/networks_edm2.py
@@ -17,7 +17,7 @@ import einops
 
 from . import misc
 from .RoPe import RotaryEmbedding
-from .attention_masking import make_train_mask, make_infer_mask
+from .attention_masking import make_train_mask, make_infer_mask, AutoregressiveDiffusionMask
 #----------------------------------------------------------------------------
 # Normalize given tensor to unit magnitude with respect to the given
 # dimensions. Default = all dimensions except the first.
@@ -102,7 +102,7 @@ class MPConv(torch.nn.Module):
         if self.training:
             with torch.no_grad():
                 self.weight.copy_(normalize(w)) # forced weight normalization
-        w = normalize(w) # traditional weight normalization. why doing it twice?
+        w = normalize(w) # traditional weight normalization. for the gradients 
         w = w * (gain / np.sqrt(w[0].numel())) # magnitude-preserving scaling
         w = w.to(x.dtype)
         if w.ndim == 2:
@@ -126,6 +126,8 @@ class VideoSelfAttention(torch.nn.Module):
         self.attn_proj = MPConv(channels, channels, kernel=[1,1]) 
         self.rope = RotaryEmbedding(channels//num_heads)
         self.block_mask = None
+        self.training_mask = None        
+    
 
     def forward(self, x, batch_size):
         if self.num_heads == 0:
@@ -133,10 +135,11 @@ class VideoSelfAttention(torch.nn.Module):
 
         h, w = x.shape[-2:]
         self.image_size = h*w
-        if self.block_mask is None or self.last_x_shape != x.shape or self.last_modality != self.training:
+        if (self.training_mask is None and self.block_mask is None) or self.last_x_shape != x.shape or self.last_modality != self.training:
             # This can trigger a recompilation of the flex_attention function
             if self.training:
                 n_frames = x.shape[0]//(batch_size*2)
+                self.training_mask = AutoregressiveDiffusionMask(n_frames, self.image_size)
                 self.block_mask = make_train_mask(batch_size, self.num_heads, n_frames, image_size=h*w)
             else:
                 n_frames = x.shape[0]//batch_size
@@ -163,15 +166,19 @@ class VideoSelfAttention(torch.nn.Module):
     
     # To log all recompilation reasons, use TORCH_LOGS="recompiles" or torch._logging.set_logs(dynamo=logging.INFO)
     @torch.compile
-    def flex_attention(self, q, k, v, block_mask, score_mod = None): 
+    def flex_attention(self, q, k, v, block_mask): 
+        score_mod = None
         if block_mask == None:
             if self.training:
-                raise NotImplementedError("Training mode not implemented")
+                def causal_mask(score, b, h, q_idx, kv_idx):
+                    return torch.where(self.training_mask(b, h, q_idx, kv_idx), score, -float("inf"))
+                score_mod = causal_mask
             else:
                 def causal_mask(score, b, h, q_idx, kv_idx):
                     q_idx, kv_idx = q_idx // self.image_size, kv_idx // self.image_size
                     return torch.where(q_idx >= kv_idx, score, -float("inf"))
                 score_mod = causal_mask
+        assert score_mod is not None or block_mask is not None, "Either block_mask or score_mod must be defined"
 
         return flex_attention(q, k, v, score_mod, block_mask)
 
@@ -271,7 +278,7 @@ class UNet(torch.nn.Module):
         cemb = model_channels * channel_mult_emb if channel_mult_emb is not None else max(cblock)
         self.label_balance = label_balance
         self.concat_balance = concat_balance
-        self.out_gain = torch.nn.Parameter(torch.tensor([1.]))
+        # self.out_gain = torch.nn.Parameter(torch.tensor([1.], requires_grad=False))
 
         # Embedding.
         self.emb_fourier = MPFourier(cnoise)
@@ -334,7 +341,7 @@ class UNet(torch.nn.Module):
             if 'block' in name:
                 x = mp_cat(x, skips.pop(), t=self.concat_balance)
             x = block(x, emb, batch_size=batch_size)
-        x = self.out_conv(x, gain=self.out_gain)
+        x = self.out_conv(x)
         x = einops.rearrange(x, '(b t) c h w -> b t c h w', b=batch_size)
         return x
 

training.py
@@ -20,11 +20,7 @@ class EncodedVideoDataset(Dataset):
         self.latent_dir = latent_dir
         self.latent_files = [f for f in os.listdir(latent_dir) if f.endswith('.pt')]
         self.latent_files.sort()
-        # self.latents_mean = (-0.06730895953510081, -0.038011381506090416, -0.07477820912866141, -0.05565264470995561, 0.012767231469026969, -0.04703542746246419, 0.043896967884726704, -0.09346305707025976, -0.09918314763016893, -0.008729793427399178, -0.011931556316503654, -0.0321993391887285)
-        # self.latents_std = (0.9263795028493863, 0.9248894543193766, 0.9393059390890617, 0.959253732819592, 0.8244560132752793, 0.917259975397747, 0.9294154431013696, 1.3720942357788521, 0.881393668867029, 0.9168315692124348, 0.9185249279345552, 0.9274757570805041)
-        
-        # self.latents_mean = torch.tensor(self.latents_mean)[None, :, None, None]
-        # self.latents_std  = torch.tensor(self.latents_std)[None, :, None, None]
+
         self.scaling_factor = 1.15
     def __len__(self):
         return len(self.latent_files)
@@ -37,8 +33,8 @@ class EncodedVideoDataset(Dataset):
         stds = (torch.randn_like(logvar) * torch.exp(0.5*logvar))
 
         out = means + stds
-        out = out / self.scaling_factor 
-        return out
+        out = out / self.scaling_factor
+        return out#-out.mean()
 
 # Example usage:
 img_resolution = 128
@@ -63,12 +59,12 @@ unet = UNet(img_resolution=img_resolution, # Match your latent resolution
             channel_mult=[1,2,4,8],
             channel_mult_noise=None,
             channel_mult_emb=None,
-            num_blocks=3,
+            num_blocks=1,
             )
 print(f"Number of UNet parameters: {sum(p.numel() for p in unet.parameters())//1e6}M")
 sigma_data = 1.
 precond = Precond(unet, use_fp16=True, sigma_data=sigma_data).to("cuda")
-loss_fn = EDM2Loss(P_mean=0.,P_std=1., sigma_data=sigma_data, noise_weight=MultiNoiseLoss())
+loss_fn = EDM2Loss(P_mean=-.4,P_std=1, sigma_data=sigma_data, noise_weight=MultiNoiseLoss())
 loss_fn.noise_weight.loss_mean_popt =[0.2,0,1,0] 
 loss_fn.noise_weight.loss_std_popt = [10,0.01,1e-4]
 
@@ -79,7 +75,7 @@ unet_params = unet.parameters()  # Get parameters from self.unet
 
 optimizer = torch.optim.AdamW(precond.parameters(), lr=1e-3)
 
-num_epochs = 10 # Adjust as needed
+num_epochs = 500 # Adjust as needed
 
 #%%
 # torch.autograd.set_detect_anomaly(True, check_nan=True)
@@ -94,6 +90,7 @@ for epoch in range(num_epochs):
         # Calculate loss    
         loss = loss_fn(precond, batch, use_loss_weight=ulw)
 
+
         # Backpropagation and optimization
         loss.backward()
         optimizer.step()
@@ -101,10 +98,11 @@ for epoch in range(num_epochs):
     print(f"Epoch: {epoch+1}/{num_epochs}, Batch: {i+1}/{len(dataloader)}, Loss: {loss.item():.4f}")
 
     # Save model checkpoint (optional)
-    if epoch % (num_epochs // 5) == 0 and epoch!=0:  # save every 20% of epochs
-        loss_fn.noise_weight.fit_loss_curve()
+    if epoch % 3 == 0:
         loss_fn.noise_weight.plot('plot.png')
-        # ulw=True
+        loss_fn.noise_weight.fit_loss_curve()
+    if epoch % (num_epochs // 5) == 0 and epoch!=0:  # save every 20% of epochs
+        ulw=True
         torch.save({
             'epoch': epoch,
             'model_state_dict': precond.state_dict(),