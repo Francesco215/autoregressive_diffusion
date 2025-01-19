f0d69005a14cfd01cbc06530e67beb2b33491fef
Francesco215 <f.sacco@protonmail.com>
2025-01-19 11:04:54 +0000

    now train.py is half decent and has some sane defaults for a Nvidia 4090

--- training.py
+++ training.py
@@ -1,6 +1,7 @@
 #%%
-import sys
 from tqdm import tqdm
+import numpy as np
+
 import torch
 from matplotlib import pyplot as plt
 
@@ -14,10 +15,11 @@ from edm2.phema import PowerFunctionEMA
 # import logging
 # torch._logging.set_logs(dynamo=logging.INFO)
 
+torch._dynamo.config.recompile_limit = 100
 # Example usage:
-micro_batch_size = 12 
-accumulation_steps = 8
-batch_size = micro_batch_size * accumulation_steps
+micro_batch_size = 4 
+batch_size = 128
+accumulation_steps = batch_size//micro_batch_size
 total_number_of_batches = 1000
 total_number_of_steps = total_number_of_batches * accumulation_steps
 
@@ -31,11 +33,11 @@ dataloader = OpenVidDataloader(micro_batch_size, num_workers, device)
 unet = UNet(img_resolution=64, # Match your latent resolution
             img_channels=16, # Match your latent channels
             label_dim = 0,
-            model_channels=64,
-            channel_mult=[1,2,4,8],
+            model_channels=128,
+            channel_mult=[1,2,2,4],
             channel_mult_noise=None,
             channel_mult_emb=None,
-            num_blocks=1,
+            num_blocks=3,
             )
 print(f"Number of UNet parameters: {sum(p.numel() for p in unet.parameters())//1e6}M")
 sigma_data = 1.
@@ -53,30 +55,31 @@ ref_lr = 1e-2
 optimizer = MARS(precond.parameters(), lr=ref_lr, eps = 1e-4)
 optimizer.zero_grad()
 
-ema_tracker = PowerFunctionEMA(precond, stds=[0.050, 0.100])
+# ema_tracker = PowerFunctionEMA(precond, stds=[0.050, 0.100])
 
 #%%
 # torch.autograd.set_detect_anomaly(True, check_nan=True)
 # Training loop
 ulw=False
-loss_tracking = 0
-for i, micro_batch in tqdm(enumerate(dataloader),total=total_number_of_steps):
+losses = []
+pbar = tqdm(enumerate(dataloader),total=total_number_of_steps)
+for i, micro_batch in pbar:
     latents = micro_batch['latents'].to(device)
 
     # Calculate loss    
     loss = loss_fn(precond, latents, use_loss_weight=ulw)
-    loss_tracking += loss.item()
+    losses.append(loss.item())
 
 
     # Backpropagation and optimization
     loss.backward()
+    pbar.set_postfix_str(f"Loss: {np.mean(losses[-accumulation_steps:]):.4f}")
     if i % accumulation_steps == 0 and i!=0:
         #microbatching
         optimizer.step()
         optimizer.zero_grad()
-        tqdm.write(f"Batch: {i}, Loss: {loss_tracking/accumulation_steps:.4f}")
         loss_tracking = 0
-        ema_tracker.update(cur_nimg= i * batch_size, batch_size=batch_size)
+        # ema_tracker.update(cur_nimg= i * batch_size, batch_size=batch_size)
 
         for g in optimizer.param_groups:
             g['lr'] = learning_rate_schedule(i//accumulation_steps, ref_lr, total_number_of_batches/2, 0)
@@ -85,13 +88,21 @@ for i, micro_batch in tqdm(enumerate(dataloader),total=total_number_of_steps):
     if i % 200 * accumulation_steps == 0:
         loss_fn.noise_weight.plot('plot.png')
         loss_fn.noise_weight.fit_loss_curve()
+        plt.plot(losses)
+        plt.xscale('log')
+        plt.xlabel('microbatch')
+        plt.ylabel('loss')
+        plt.title('Losses')
+        plt.savefig('losses.png')
+        plt.show()
+        plt.close()
 
     if i % (total_number_of_steps//5) == 0 and i!=0:  # save every 20% of epochs
         torch.save({
             'batch': i,
             'model_state_dict': precond.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'ema_state_dict': ema_tracker.state_dict(),
+            # 'ema_state_dict': ema_tracker.state_dict(),
             'loss': loss,
         }, f"model_batch_{i+1}.pt")
 
