abbbfdba24b8b05d20c996364345499e7b25721c
Francesco215 <f.sacco@protonmail.com>
2025-01-22 10:54:58 +0000

    improved a bit the setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 apt update
-apt install btop tmux unzip ffmpeguv  -y
+apt install btop tmux unzip ffmpeg  -y
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf awscliv2.zip aws/
+export AWS_PROFILE=wasabi
 
 pip install swig uv nvitop 
 git config --global user.email "f.sacco@protonmail.com"
 git config --global user.name "Francesco215"
 alias auv='uv venv; . .venv/bin/activate'
+
 uv venv 
 . .venv/bin/activate
 uv pip install -e .
