fixed some bugs, renamed files, and made sure that the 3d cond is variance-preserving

edm2/conv_3d.py
@@ -1,7 +1,7 @@
 import einops
 import torch
 import numpy as np
-from .networks_edm2 import normalize
+from .utils import normalize
 # Magnitude-preserving convolution or fully-connected layer (Equation 47)
 # with force weight normalization (Equation 66).
 
@@ -21,8 +21,8 @@ class MPCausal3DConv(torch.nn.Module):
         w = w.to(x.dtype)
         assert w.ndim == 5
 
-        padding = (0, w.shape[1]//2, w.shape[2]//2)
-        causal_pad = torch.ones(batch_size, w.shape[2]-1, *context.shape[2:], device=x.device, dtype=x.dtype)
+        padding = (0, w.shape[-2]//2, w.shape[-1]//2)
+        causal_pad = torch.ones(batch_size, x.shape[1], w.shape[2]-1, *x.shape[2:], device=x.device, dtype=x.dtype)
 
         if self.training:
             # Warning: to understand this, read first how it works during inference
@@ -33,17 +33,17 @@ class MPCausal3DConv(torch.nn.Module):
             # or we use the fact that the conv layers are linear (good). 
             
             # we do the 2d convolutions over the last frames
-            last_frame_conv = torch.nn.functional.conv2d(context, w=w[:,:,-1],  padding=w.shape[-1]//2)
+            last_frame_conv = torch.nn.functional.conv2d(x, w[:,:,-1],  padding=padding[1:])
 
             # we just take the context frames
             context, _ = einops.rearrange(x, '(b s t) c h w -> s b c t h w', b=batch_size, s=2).unbind(0)
             #pad context along the time dimention to make sure that it's causal
-            context = torch.cat(causal_pad, context, dim = 1)
+            context = torch.cat((causal_pad, context), dim=-3)
 
             # now we do the 3d convolutions over the previous frames of the context
-            context = torch.nn.functional.conv3d(context, w=w[:,:,:-1], padding=padding)[:,:,:-1]
+            context = torch.nn.functional.conv3d(context, w[:,:,:-1], padding=padding)[:,:,:-1]
             # we concatenate the results and reshape them to sum them back to the 2d convolutions
-            context = torch.cat((context, context), dim = 0)
+            context = torch.stack((context, context), dim=0)
             context = einops.rearrange(context, 's b c t h w -> (b s t) c h w')
 
             # we use the fact that the convolution is linear to sum the results of the 2d and 3d convolutions

edm2/networks_edm2.py
@@ -18,16 +18,8 @@ import einops
 from . import misc
 from .RoPe import RotaryEmbedding
 from .attention_masking import make_train_mask, make_infer_mask, AutoregressiveDiffusionMask
-#----------------------------------------------------------------------------
-# Normalize given tensor to unit magnitude with respect to the given
-# dimensions. Default = all dimensions except the first.
-
-def normalize(x, dim=None, eps=1e-4):
-    if dim is None:
-        dim = list(range(1, x.ndim))
-    norm = torch.linalg.vector_norm(x, dim=dim, keepdim=True, dtype=torch.float32)
-    norm = torch.add(eps, norm, alpha=np.sqrt(norm.numel() / x.numel()))
-    return x / norm.to(x.dtype)
+from .utils import normalize
+from .conv_3d import MPCausal3DConv
 
 #----------------------------------------------------------------------------
 # Upsample or downsample the given tensor with the given filter,
@@ -212,9 +204,9 @@ class Block(torch.nn.Module):
         self.attn_balance = attn_balance
         self.clip_act = clip_act
         self.emb_gain = torch.nn.Parameter(torch.zeros([]))
-        self.conv_res0 = MPConv(out_channels if flavor == 'enc' else in_channels, out_channels, kernel=[3,3])
+        self.conv_res0 = MPCausal3DConv(out_channels if flavor == 'enc' else in_channels, out_channels, kernel=[3,3,3])
         self.emb_linear = MPConv(emb_channels, out_channels, kernel=[])
-        self.conv_res1 = MPConv(out_channels, out_channels, kernel=[3,3])
+        self.conv_res1 = MPCausal3DConv(out_channels, out_channels, kernel=[3,3,3])
         self.conv_skip = MPConv(in_channels, out_channels, kernel=[1,1]) if in_channels != out_channels else None
         self.attn = VideoSelfAttention(out_channels, self.num_heads, attn_balance)
 
@@ -230,13 +222,13 @@ class Block(torch.nn.Module):
             x = normalize(x, dim=1) # pixel norm
 
         # Residual branch.
-        y = self.conv_res0(mp_silu(x))
+        y = self.conv_res0(mp_silu(x),batch_size=batch_size)
         c = self.emb_linear(emb, gain=self.emb_gain) + 1
         y = einops.einsum(y, c.to(y.dtype),'b c h w, b c -> b c h w') 
         y = mp_silu(y)
         if self.training and self.dropout != 0:
             y = torch.nn.functional.dropout(y, p=self.dropout)
-        y = self.conv_res1(y)
+        y = self.conv_res1(y, batch_size=batch_size)
 
         # Connect the branches.
         if self.flavor == 'dec' and self.conv_skip is not None: