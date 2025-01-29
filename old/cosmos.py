3684cb14482b380d1694909b9beafd3bd366004d
Francesco215 <f.sacco@protonmail.com>
2025-01-29 18:52:03 +0000

    removed some useless files

---
+++ old/cosmos.py
@@ -0,0 +1,28 @@
+#%%
+import io
+import torch
+from typing import Optional
+from datasets import load_dataset
+from edm2.dataloading import OpenVidDataloader
+
+batch_size = 16
+dataloader = OpenVidDataloader(batch_size=batch_size, num_workers=32, device="cuda")
+
+mean_sum = 0
+std_sum = 0
+num_batches = 0
+
+for i, data in enumerate(dataloader):
+    images = data["latents"].to("cuda")
+    mean_sum += images.mean()
+    std_sum += images.std()#dim=-3).mean()
+    num_batches += i
+    if i == 20:
+        break
+
+mean = mean_sum / num_batches
+std = std_sum / num_batches
+
+print(f"Mean: {mean}, Std: {std}")
+
+# %%
