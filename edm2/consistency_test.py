cf76db97e0d6257408128f296a0778423a8ed88d
Francesco215 <f.sacco@protonmail.com>
2025-01-30 19:12:19 +0000

    improved tests, they seem to work

--- edm2/consistency_test.py
+++ edm2/consistency_test.py
@@ -2,8 +2,9 @@ import torch
 import unittest
 import logging
 import os
-from edm2.networks_edm2 import UNet, Precond
-from edm2.loss import EDM2Loss
+from edm2.networks_edm2 import UNet
+import numpy as np
+import random 
 
 os.environ['TORCH_LOGS']='recompiles'
 os.environ['TORCH_COMPILE_MAX_AUTOTUNE_RECOMPILE_LIMIT']='100000'
@@ -16,13 +17,14 @@ IMG_CHANNELS = 16
 N_FRAMES = 8
 CUT_FRAME = 3
 SEED = 42  # Set a seed for reproducibility
-
+dtype = torch.float32
+np.random.seed(42)
+random.seed(42)
 class TestUNet(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         torch.manual_seed(SEED)
-        if torch.cuda.is_available():
-            torch.cuda.manual_seed_all(SEED)
+        torch.cuda.manual_seed_all(SEED)
 
         # Initialize UNet and Precond
         cls.unet = UNet(
@@ -30,45 +32,45 @@ class TestUNet(unittest.TestCase):
             img_channels=IMG_CHANNELS,
             label_dim=0,
             model_channels=32,
-            channel_mult=[1, 2, 2, 4],
+            channel_mult=[1, 2, 2],
             channel_mult_noise=None,
             channel_mult_emb=None,
-            num_blocks=2,
-            attn_resolutions=[16, 8]
-        ).to("cuda").to(torch.float16)
+            num_blocks=1,
+            attn_resolutions=[]
+        ).to("cuda").to(dtype)
         print(f"Number of UNet parameters: {sum(p.numel() for p in cls.unet.parameters()) // 1e6}M")
 
     
     def test_unet_consistency_between_train_and_eval(self):
         # Test consistency between training and evaluation modes
         self.unet.train()
-        x = torch.randn(2, 2 * N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=torch.float16)
-        noise_level = torch.zeros(x.shape[:2], device="cuda", dtype=torch.float16)
+        x = torch.randn(2, 2 * N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        noise_level = torch.zeros(x.shape[:2], device="cuda", dtype=dtype)
         y_train = self.unet.forward(x, noise_level, None)
 
         self.unet.eval()
         x_eval = torch.cat((x[:, :CUT_FRAME], x[:, CUT_FRAME + N_FRAMES].unsqueeze(1)), dim=1)
         noise_level_eval = torch.cat((noise_level[:, :CUT_FRAME], noise_level[:, CUT_FRAME + N_FRAMES].unsqueeze(1)), dim=1)
         y_eval = self.unet.forward(x_eval, noise_level_eval, None)
-
-        self.assertTrue(torch.allclose(y_train[:, :CUT_FRAME], y_eval[:, :-1], atol=1e-2))
-        self.assertTrue(torch.allclose(y_train[:, CUT_FRAME + N_FRAMES], y_eval[:, -1], atol=1e-2))
+        
+        self.assertTrue(((y_train[:,:CUT_FRAME]-y_eval[:,:-1]).std()<4e-4).item())
+        self.assertTrue(((y_train[:,CUT_FRAME+N_FRAMES] - y_eval[:,-1]).std()<4e-4).item())
 
     def test_unet_causality(self):
         # make sure that during training the network is fully causal
         self.unet.train()
-        x = torch.zeros(2, N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=torch.float16)
-        r = torch.randn(2, N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=torch.float16)
+        x = torch.zeros(2, N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        r = torch.randn(2, N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
         a = torch.cat((x, r), dim=1)
-        x[:, CUT_FRAME] = torch.randn(x.shape[0], IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=torch.float16)
+        x[:, CUT_FRAME] = torch.randn(x.shape[0], IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
         x = torch.cat((x, r), dim=1)
-        noise_level = torch.zeros(x.shape[:2], device="cuda", dtype=torch.float16)
+        noise_level = torch.zeros(x.shape[:2], device="cuda", dtype=dtype)
         y = self.unet.forward(x, noise_level, None) - self.unet.forward(a, noise_level, None)
 
-        self.assertTrue(torch.allclose(y[:, :CUT_FRAME].std(), torch.tensor(0, dtype=torch.float16), atol=1e-2))
-        self.assertFalse(torch.allclose(y[:, CUT_FRAME:N_FRAMES].std(), torch.tensor(0, dtype=torch.float16), atol=1e-2))
-        self.assertTrue(torch.allclose(y[:, N_FRAMES:N_FRAMES + CUT_FRAME].std(), torch.tensor(0, dtype=torch.float16), atol=1e-2))
-        self.assertFalse(torch.allclose(y[:, N_FRAMES + CUT_FRAME:].std(), torch.tensor(0, dtype=torch.float16), atol=1e-2))
+        self.assertTrue((y[:, :CUT_FRAME].std()<4e-4).item())
+        self.assertTrue((y[:, CUT_FRAME:N_FRAMES].std()>0.3).item())
+        self.assertTrue((y[:, N_FRAMES:N_FRAMES + CUT_FRAME].std()<4e-4).item())
+        self.assertFalse((y[:, N_FRAMES + CUT_FRAME:].std()>0.3).item())
 
 if __name__ == "__main__":
     unittest.main()
