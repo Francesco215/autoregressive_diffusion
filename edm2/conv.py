fb88be420db1e2c4acd471b8d21396acc5fbf4f7
Francesco215 <f.sacco@protonmail.com>
2025-02-03 23:58:00 +0000

    minor

--- edm2/conv.py
+++ edm2/conv.py
@@ -87,16 +87,9 @@ class MPCausal3DConv(torch.nn.Module):
         self.out_channels = out_channels
         assert len(kernel)==3
         self.weight = EfficientWeight(in_channels, out_channels, kernel)
-        # self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels, *kernel))
 
     def forward(self, x, batch_size, gain=1):
-        # w = self.weight.to(torch.float32)
-        # if self.training:
-        #     with torch.no_grad():
-        #         self.weight.copy_(normalize(w)) # forced weight normalization
-        # w = normalize(w) # traditional weight normalization. for the gradients 
         w = self.weight().to(x.dtype)
-
         image_padding = (0, w.shape[-2]//2, w.shape[-1]//2)
 
         # TODO: fix this
@@ -119,21 +112,17 @@ class MPCausal3DConv(torch.nn.Module):
             context, _ = einops.rearrange(x, '(b s t) c h w -> s b c t h w', b=batch_size, s=2).unbind(0)
             #pad context along the time dimention to make sure that it's causal
             context = torch.cat((causal_pad, context), dim=-3)
-            # context.std(dim=(0,2,3,4)) = [0.9905, 0.9973, 1.0003, 0.9994, 0.9943, 1.0005, 1.0013, 0.9964, 0.9992, 0.9994, 0.9981, 1.0032, 1.0023, 1.0002, 1.0015, 0.9988]
 
             # now we do the 3d convolutions over the previous frames of the context
             context = torch.nn.functional.conv3d(context[:,:,:-1], w[:,:,:-1], padding=image_padding)
-            # context.std(dim=(0,2,3,4)) = [1.1555, 1.0016, 0.9064, 1.0799, 0.8134, 0.7813, 0.7638, 0.8903, 0.8006, 0.7442, 0.7492, 0.8073, 0.8076, 0.7287, 0.7323, 0.7740]
 
             # we concatenate the results and reshape them to sum them back to the 2d convolutions
-            # c_log = context.clone()
             context = torch.stack((context, context), dim=0)
             context = einops.rearrange(context, 's b c t h w -> (b s t) c h w')
 
             # we use the fact that the convolution is linear to sum the results of the 2d and 3d convolutions
             x = context + last_frame_conv
             x = x * (gain / np.sqrt(w[0].numel())) # magnitude-preserving scaling
-            # to_log = einops.rearrange(x, '(b t) ... -> b t ...', b = batch_size)
             return x
 
         # during inference is much simpler
