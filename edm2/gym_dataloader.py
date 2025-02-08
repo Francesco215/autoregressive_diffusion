7c4ccbb5ab6044e057c446cd19cac7873b84bb20
Francesco215 <f.sacco@protonmail.com>
2025-02-08 15:20:58 +0000

    added code to train diffusion model on gym env

---
+++ edm2/gym_dataloader.py
@@ -0,0 +1,98 @@
+import torch
+from torch import Tensor
+from torch.utils.data import IterableDataset
+
+import cv2
+import einops
+import gymnasium as gym
+
+
+class GymDataGenerator(IterableDataset):
+    def __init__(self, state_size=6, environment_name="CartPole-v1", training_examples=10_000):
+        self.state_size = state_size
+
+        self.env = gym.make(environment_name,render_mode="rgb_array")
+
+        self.evolution_time = 10
+        self.training_examples = training_examples
+
+    def __iter__(self):
+        terminated = True
+
+        for _ in range(self.training_examples):
+            if terminated:
+                observation, _ = self.env.reset()
+                terminated = False
+                reward = 0
+                action = 0
+                frame_history = []
+                # action_history = []
+                step_count = -self.evolution_time
+            else:
+                action = self.env.action_space.sample()  # Random action
+                _ , reward, terminated, _, _ = self.env.step(action)
+            
+            # action_history.append(action)
+            # action_history = action_history[-self.state_size:]
+
+            if step_count >= 0: # This if can be removed, but having it avoids rendering useless frames
+                frame = self.env.render()
+                frame = resize_image(frame)
+                # frame_image = Image.fromarray(frame)
+                frame_history.append(torch.tensor(frame))
+                frame_history = frame_history[-self.state_size:]
+            
+            if step_count > 0 and step_count%self.state_size==0:  # Skip the first step as we don't have a previous state
+                assert len(frame_history)>=self.state_size
+                frames = torch.stack(frame_history[-self.state_size:])
+
+                actions = torch.tensor(action).clone()
+                # if step_count%self.evolution_time==self.evolution_time-1:
+
+                yield frames, actions, torch.tensor(reward).clone()
+                frame_history = []
+                
+            step_count += 1
+
+
+def resize_image(image_array):
+    # Check if the input array has the correct shape
+    if image_array.shape != (400, 600, 3):
+        raise ValueError("Input array must have shape (400, 600, 3)")
+    
+    # Resize the image using OpenCV
+    resized_image = cv2.resize(image_array, (256, 256), interpolation=cv2.INTER_AREA)
+    
+    return resized_image 
+def gym_collate_function(batch):
+    frame_histories, action_histories, rewards = zip(*batch)
+    padded_frames = torch.stack(frame_histories)
+    padded_actions = torch.stack(action_histories)
+    
+    return padded_frames, padded_actions, torch.Tensor(rewards)
+
+    
+@torch.no_grad()
+def frames_to_latents(autoencoder, frames)->Tensor:
+    """
+    frames.shape: (batch_size, time, height, width, rgb)
+    latents.shape: (batch_size, time, latent_channels, latent_height, latent_width)
+    """
+    batch_size = frames.shape[0]
+
+    frames = frames / 127.5 - 1  # Normalize from (0,255) to (-1,1)
+    frames = einops.rearrange(frames, 'b t h w c -> (b t) c h w')
+
+    #split the conversion to not overload the GPU RAM
+    split_size = 64
+    for i in range (0, frames.shape[0], split_size):
+        l = autoencoder.encode(frames[i:i+split_size]).latent_dist.sample()
+        if i == 0:
+            latents = l
+        else:
+            latents = torch.cat((latents, l), dim=0)
+
+    # Apply scaling factor
+    latents = latents * autoencoder.config.scaling_factor
+    latents = einops.rearrange(latents, '(b t) c h w -> b t c h w', b=batch_size)
+    return latents
---
+++ gym_training.py
@@ -0,0 +1,110 @@
+#%%
+import numpy as np
+import torch
+from torch import nn, optim, Tensor
+from torch.utils.data import DataLoader
+
+import einops
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from diffusers import AutoencoderKL
+
+
+from edm2.gym_dataloader import GymDataGenerator, gym_collate_function, frames_to_latents
+from edm2.networks_edm2 import UNet, Precond
+from edm2.loss import EDM2Loss, learning_rate_schedule
+from edm2.loss_weight import MultiNoiseLoss
+from edm2.mars import MARS
+from edm2.phema import PowerFunctionEMA
+
+#%%
+if __name__=="__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    original_env = "LunarLander-v3"
+    model_id="stabilityai/stable-diffusion-2-1"
+
+    autoencoder = AutoencoderKL.from_pretrained(model_id, subfolder="vae").to(device).requires_grad_(False)
+    latent_channels = autoencoder.config.latent_channels
+
+    unet = UNet(img_resolution=32, # Match your latent resolution
+                img_channels=latent_channels, # Match your latent channels
+                label_dim = 0,
+                model_channels=64,
+                channel_mult=[1,2,2,4],
+                channel_mult_noise=None,
+                channel_mult_emb=None,
+                num_blocks=1,
+                attn_resolutions=[16,8]
+                )
+
+    micro_batch_size = 4
+    batch_size = 64
+    accumulation_steps = batch_size//micro_batch_size
+    state_size = 8 
+    total_number_of_steps = 10_000
+    training_steps = total_number_of_steps * batch_size
+    dataset = GymDataGenerator(state_size, original_env, training_steps)
+    dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=gym_collate_function, num_workers=2)
+
+    unet_params = sum(p.numel() for p in unet.parameters())
+    print(f"Number of UNet parameters: {unet_params//1e6}M")
+    # sigma_data = 0.434
+    sigma_data = 1.
+    precond = Precond(unet, use_fp16=True, sigma_data=sigma_data).to(device)
+    loss_fn = EDM2Loss(P_mean=0.5,P_std=1.5, sigma_data=sigma_data, noise_weight=MultiNoiseLoss())
+
+    ref_lr = 1e-2
+    current_lr = ref_lr
+    optimizer = MARS(precond.parameters(), lr=ref_lr, eps = 1e-4)
+    optimizer.zero_grad()
+
+    ema_tracker = PowerFunctionEMA(precond, stds=[0.050, 0.100])
+    losses = []
+    #%%
+    ulw=False
+    pbar = tqdm(enumerate(dataloader),total=total_number_of_steps)
+    for i, batch in pbar:
+        frames, action, reward = batch
+        frames = frames.to(device)
+        action = action.to(device)
+        # action_emb = diffusion.action_embedder(action)
+
+        latents = frames_to_latents(autoencoder, frames)
+
+        # Calculate loss    
+        loss, un_weighted_loss = loss_fn(precond, latents, use_loss_weight=ulw)
+        losses.append(un_weighted_loss)
+        # Backpropagation and optimization
+        loss.backward()
+        pbar.set_postfix_str(f"Loss: {np.mean(losses[-accumulation_steps:]):.4f}, lr: {current_lr:.4f}")
+
+        if i % accumulation_steps == 0 and i!=0:
+            #microbatching
+            optimizer.step()
+            optimizer.zero_grad()
+            ema_tracker.update(cur_nimg= i * batch_size, batch_size=batch_size)
+
+            for g in optimizer.param_groups:
+                current_lr = learning_rate_schedule(i, ref_lr, total_number_of_steps/100, 0)
+                g['lr'] = current_lr
+
+        # Save model checkpoint (optional)
+        if i % 50 * accumulation_steps == 0 and i!=0:
+            loss_fn.noise_weight.fit_loss_curve()
+            loss_fn.noise_weight.plot('plot.png')
+            n_clips = np.linspace(0, i * micro_batch_size, len(losses))
+            plt.plot(n_clips, losses, label='Loss', color='blue', alpha=0.5)
+            if len(losses) >= 100:
+                moving_avg = np.convolve(losses, np.ones(100) / 100, mode='valid')
+                n_images_avg = np.linspace(0, i * micro_batch_size, len(moving_avg))
+                plt.plot(n_images_avg, moving_avg, label='Moving Average', color='blue', alpha=1)
+            plt.xscale('log')
+            plt.xlabel('n images')
+            plt.ylabel('loss')
+            plt.title(f'Losses with {unet_params} parameters')
+            plt.savefig('losses.png')
+            plt.show()
+            plt.close()
+            ulw=True
