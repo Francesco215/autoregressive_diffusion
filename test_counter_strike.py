80df23b7f916ba2ccf0e7be3047326767cbf9b0e
Francesco215 <f.sacco@protonmail.com>
2025-02-27 17:01:06 +0000

    minor

--- test_counter_strike.py
+++ test_counter_strike.py
@@ -18,8 +18,8 @@ from huggingface_hub import hf_hub_download
 
 autoencoder = AutoencoderKLMochi.from_pretrained("genmo/mochi-1-preview", subfolder="vae", torch_dtype=torch.float32).to("cuda").requires_grad_(False)
 
-repo_id="TeaPearce/CounterStrike_Deathmatch"
-filename = "dataset_aim_expert.tar"
+hf_repo_id="TeaPearce/CounterStrike_Deathmatch"
+hf_filename = "dataset_aim_expert.tar"
 #%%
 # Load with h5py
 def read_frames_and_actions(filename):
@@ -61,11 +61,11 @@ def encode_frames(autoencoder, frames, actions):
     for mean, logvar, action in zip(means, logvars, actions):
         yield {"mean": mean, "logvar": logvar, "action": action}
 
-def compress_huggingface_filename(repo_id, filename):
-    tar_file_path = hf_hub_download(repo_id, filename, repo_type = "dataset")
+def compress_huggingface_filename(hf_repo_id, hf_filename):
+    tar_file_path = hf_hub_download(hf_repo_id, hf_filename, repo_type = "dataset")
 
     # Extract the downloaded .tar file
-    save_folder = f"/tmp/{filename.split('.')[0]}"
+    save_folder = f"/tmp/{hf_filename.split('.')[0]}"
     with tarfile.open(tar_file_path, "r") as tar:
         tar.extractall("/tmp/")  
 
@@ -90,5 +90,5 @@ def write_mds(hf_repo_id, hf_filename, mds_dirname):
             writer.write(encoded_frame)
 
 #%%
-write_mds(repo_id, filename, "counter_strike")
+write_mds(hf_repo_id, hf_filename, "counter_strike")
 # %%
