62d1a6d9aa56fa16ffdf269ee0534b05e650c2df
Francesco215 <f.sacco@protonmail.com>
2025-03-23 23:24:40 +0000

    minor

--- setup.sh
+++ setup.sh
@@ -5,15 +5,13 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 rm -rf awscliv2.zip aws/
-# export AWS_PROFILE=wasabi
 
-pip install swig uv nvitop 
 git config --global user.email "f.sacco@protonmail.com"
 git config --global user.name "Francesco215"
+
+pip install swig uv nvitop 
 alias auv='uv venv; . .venv/bin/activate'
 
 uv venv 
 . .venv/bin/activate
 uv pip install -e .
-
-# uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126
