b53a4cb0e0c314f976a1315c855a526fc7dc49ec
the-puzzler <172930174+the-puzzler@users.noreply.github.com>
2025-05-07 17:55:51 +0000

    minor

--- asd.py
+++ asd.py
@@ -1,7 +1,7 @@
 #%%
 from diffusers import AutoencoderKL
 import torch
-
+from edm2.vae import VAE
 
 vae2d = AutoencoderKL.from_pretrained("THUDM/CogView4-6B", subfolder="vae")
 x = torch.randn(2,1024,16,16)                       # (B, C, T, H, W)
@@ -12,3 +12,6 @@ vae2d
 
 
 # %%
+
+vae = VAE(8)
+vae._load_from_2d_model(vae2d)
