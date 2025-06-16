fix test and brought back normalization

edm2/attention/attention_modules.py
@@ -36,7 +36,7 @@ class VideoAttention(nn.Module):
 
         if just_2d: #just use the code from frame attention
             y = einops.rearrange(y, 'bt (m c s) h w -> s bt m (h w) c', s=3, m=self.num_heads)
-            q, k, v =y.unbind(0)
+            q, k, v =normalize(y, dim=-1).unbind(0)
 
             y = F.scaled_dot_product_attention(q, k, v)
             y = einops.rearrange(y, 'bt m (h w) c -> bt (m c) h w', h=h, w=w)
@@ -46,7 +46,7 @@ class VideoAttention(nn.Module):
             
         # b:batch, t:time, m: multi-head, s: split, c: channels, h: height, w: width
         y = einops.rearrange(y, '(b t) (m c s) h w -> s b m t (h w) c', b=batch_size, s=3, m=self.num_heads)
-        q, k, v = normalize(y,dim=-1).unbind(0) # pixel norm & split 
+        q, k, v = normalize(y, dim=-1).unbind(0) # pixel norm & split 
 
         if not self.training: # Handling of the cache during inference
             if cache is not None:

edm2/consistency_test.py
@@ -85,14 +85,14 @@ class TestAttention(unittest.TestCase):
         repeated_mask = mask.to_dense()[0,0].bool().repeat_interleave(IMG_RESOLUTION**2,0).repeat_interleave(IMG_RESOLUTION**2,1)
 
         y = self.attention.attn_qkv(x)
-        y = einops.rearrange(y, '(b t) (s m c) h w -> s b m t (h w) c', b=BATCH_SIZE, s=3, m=4)
-        q, k, v = y.unbind(0) # pixel norm & split 
+        y = einops.rearrange(y, '(b t) (m c s) h w -> s b m t (h w) c', b=BATCH_SIZE, s=3, m=4)
+        q, k, v = normalize(y, dim=-1).unbind(0) # pixel norm & split 
 
         q, k = self.attention.rope(q, k)
         v = einops.rearrange(v, ' b m t hw c -> b m (t hw) c') # q and k are already rearranged inside of rope
         y = F.scaled_dot_product_attention(q,k,v,repeated_mask)
 
-        y = einops.rearrange(y, 'b m (t h w) c -> (b t) (c m) h w', b=BATCH_SIZE, h=IMG_RESOLUTION, w=IMG_RESOLUTION)
+        y = einops.rearrange(y, 'b m (t h w) c -> (b t) (m c) h w', b=BATCH_SIZE, h=IMG_RESOLUTION, w=IMG_RESOLUTION)
         y = self.attention.attn_proj(y)
         
         y_manual = mp_sum(x, y, t=self.attention.attn_balance)