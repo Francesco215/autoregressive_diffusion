071d5ef3ca6b34e4b1df3280fb6595e85e17c12b
Francesco215 <f.sacco@protonmail.com>
2025-08-24 12:57:04 +0000

    updated consistency testing to handle a pretrained unet

--- edm2/consistency_test.py
+++ edm2/consistency_test.py
@@ -22,9 +22,9 @@ torch._logging.set_logs(dynamo=logging.INFO)
 # Constants
 IMG_RESOLUTION = 16
 BATCH_SIZE = 4
-IMG_CHANNELS = 16
-N_FRAMES = 8
-CUT_FRAME = 3
+IMG_CHANNELS = 3
+N_FRAMES = 16
+CUT_FRAME = 4
 SEED = 42  # Set a seed for reproducibility
 dtype = torch.float32
 np.random.seed(42)
@@ -63,41 +63,31 @@ class TestAttention(unittest.TestCase):
     def setUpClass(cls):
         torch.manual_seed(SEED)
         torch.cuda.manual_seed_all(SEED)
-        cls.attention = VideoAttention(channels = 4*IMG_CHANNELS, num_heads = 4).to("cuda").to(dtype)
-
-    def test_attention_consistency_between_frame_and_video_attention(self):
-        self.attention.train()
-        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, 4*IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
-        y_video, _ = self.attention(x.clone(), BATCH_SIZE, just_2d=False)
-        y_frame, _ = self.attention(x.clone(), BATCH_SIZE, just_2d=True)
-        
-        y_video = einops.rearrange(y_video, '(b s t) c h w -> s b t c h w', b=BATCH_SIZE, s = 2)
-        y_frame = einops.rearrange(y_frame, '(b s t) c h w -> s b t c h w', b=BATCH_SIZE, s = 2)
-
-        std_diff = (y_video-y_frame).std(dim=(0,1,3,4,5))
-        self.assertLessEqual(std_diff[0].item(), error_bound, f"Test failed: std deviation {std_diff} exceeded {error_bound}") 
-        self.assertGreaterEqual(std_diff[1:].mean().item(), 1e-2, f"Test failed: std deviation {std_diff} exceeded {error_bound}") 
+        cls.unet = UNet.from_pretrained('saved_models/unet_11.0M.pt').to("cuda").to(dtype)
+        cls.attention = cls.unet.enc['8x8_block0'].attn 
+        assert isinstance(cls.attention, VideoAttention)
 
 
 
 
     def test_attention_consistency_between_flex_and_flash_video_attention(self):
         self.attention.train()
-        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, 4*IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_resolution = 16
+        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, self.attention.channels, img_resolution, img_resolution, device="cuda", dtype=dtype)
         y_video, _ = self.attention(x.clone(), BATCH_SIZE, just_2d=False)
 
-        mask = make_train_mask(BATCH_SIZE, 4, N_FRAMES, IMG_RESOLUTION**2) # TODO: check again
-        repeated_mask = mask.to_dense()[0,0].bool().repeat_interleave(IMG_RESOLUTION**2,0).repeat_interleave(IMG_RESOLUTION**2,1)
+        mask = make_train_mask(BATCH_SIZE, 4, N_FRAMES, img_resolution**2)[1] # TODO: check again
+        repeated_mask = mask.to_dense()[0,0].bool().repeat_interleave(img_resolution**2,0).repeat_interleave(img_resolution**2,1)
 
         y = self.attention.attn_qkv(x)
-        y = einops.rearrange(y, '(b t) (m c s) h w -> s b m t (h w) c', b=BATCH_SIZE, s=3, m=4)
+        y = einops.rearrange(y, '(b t) (m c s) h w -> s b m t (h w) c', b=BATCH_SIZE, s=3, m=self.attention.num_heads)
         q, k, v = normalize(y, dim=-1).unbind(0) # pixel norm & split 
 
         q, k = self.attention.rope(q, k)
         v = einops.rearrange(v, ' b m t hw c -> b m (t hw) c') # q and k are already rearranged inside of rope
         y = F.scaled_dot_product_attention(q,k,v,repeated_mask)
 
-        y = einops.rearrange(y, 'b m (t h w) c -> (b t) (m c) h w', b=BATCH_SIZE, h=IMG_RESOLUTION, w=IMG_RESOLUTION)
+        y = einops.rearrange(y, 'b m (t h w) c -> (b t) (m c) h w', b=BATCH_SIZE, h=img_resolution, w=img_resolution)
         y = self.attention.attn_proj(y)
         
         y_manual = mp_sum(x, y, t=self.attention.attn_balance)
@@ -108,9 +98,24 @@ class TestAttention(unittest.TestCase):
 
 
 
+    def test_attention_consistency_between_frame_and_video_attention(self):
+        self.attention.train()
+        img_resolution=8
+        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
+        y_video, _ = self.attention(x.clone(), BATCH_SIZE, just_2d=False)
+        y_frame, _ = self.attention(x.clone(), BATCH_SIZE, just_2d=True)
+        
+        y_video = einops.rearrange(y_video, '(b s t) c h w -> s b t c h w', b=BATCH_SIZE, s = 2)
+        y_frame = einops.rearrange(y_frame, '(b s t) c h w -> s b t c h w', b=BATCH_SIZE, s = 2)
+
+        std_diff = (y_video-y_frame).std(dim=(0,1,3,4,5))
+        self.assertLessEqual(std_diff[0].item(), error_bound, f"Test failed: std deviation {std_diff} exceeded {error_bound}") 
+        self.assertGreaterEqual(std_diff[1:].mean().item(), 1e-2, f"Test failed: std deviation {std_diff} exceeded {error_bound}") 
+
     def test_attention_consistency_between_train_and_eval(self):
         self.attention.train()
-        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, 4*IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_resolution=8
+        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
         y_train, _ = self.attention(x, BATCH_SIZE)
         
         self.attention.eval()
@@ -131,7 +136,8 @@ class TestAttention(unittest.TestCase):
 
     def test_attention_consistrency_between_cached_and_non_cached(self):
         self.attention.eval()
-        x = torch.randn(BATCH_SIZE, N_FRAMES, 4*IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_resolution=8
+        x = torch.randn(BATCH_SIZE, N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
         context, last_frame = x[:, :-1], x[:,-1:]
         x = einops.rearrange(x, 'b t ... -> (b t) ...')
         context = einops.rearrange(context, 'b t ... -> (b t) ...')
@@ -150,7 +156,8 @@ class TestAttention(unittest.TestCase):
 
     def test_attention_consistrency_between_cached_and_non_cached_multistep(self):
         self.attention.eval()
-        x = torch.randn(BATCH_SIZE, N_FRAMES, 4*IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_resolution=8
+        x = torch.randn(BATCH_SIZE, N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
         context, second_last, last_frame = x[:, :-2], x[:,-2:-1], x[:,-1:]
 
         x = einops.rearrange(x, 'b t ... -> (b t) ...')
@@ -177,10 +184,11 @@ class TestAttention(unittest.TestCase):
     def test_attention_causality(self):
         # make sure that during training the network is fully causal
         self.attention.train()
-        x = torch.zeros(BATCH_SIZE, N_FRAMES, 4 * IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
-        r = torch.randn(BATCH_SIZE, N_FRAMES, 4 * IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_resolution=8
+        x = torch.zeros(BATCH_SIZE, N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
+        r = torch.randn(BATCH_SIZE, N_FRAMES, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
         a = torch.cat((x, r), dim=1)
-        x[:, CUT_FRAME] = torch.randn(BATCH_SIZE, 4 * IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        x[:, CUT_FRAME] = torch.randn(BATCH_SIZE, self.attention.attn_qkv.weight.weight.shape[1], img_resolution, img_resolution, device="cuda", dtype=dtype)
         x = torch.cat((x, r), dim=1)
         a = einops.rearrange(a, 'b t c h w -> (b t) c h w')
         x = einops.rearrange(x, 'b t c h w -> (b t) c h w')
@@ -191,10 +199,10 @@ class TestAttention(unittest.TestCase):
         self.assertLessEqual( y[:, :CUT_FRAME].std().item(), error_bound, f"Test failed: std deviation {y[:, :CUT_FRAME].std()} exceeded {error_bound} before CUT_FRAME")
         self.assertGreaterEqual( y[:, CUT_FRAME:CUT_FRAME+1].std().item(), 0.3, f"Test failed: perturbation at CUT_FRAME did not affect output as expected")
         # TODO: CHECK THIS INFORMATION BOTTLENECK
-        self.assertGreaterEqual(  y[:, CUT_FRAME+1:N_FRAMES].std(dim=2).mean().item(), 0.1/np.sqrt(N_FRAMES*IMG_RESOLUTION**2), f"Test failed: post-CUT_FRAME frames did not respond to perturbation")
+        self.assertGreaterEqual(  y[:, CUT_FRAME+1:N_FRAMES].std(dim=2).mean().item(), 0.1/np.sqrt(N_FRAMES*img_resolution**2), f"Test failed: post-CUT_FRAME frames did not respond to perturbation")
         self.assertLessEqual( y[:, N_FRAMES:N_FRAMES + CUT_FRAME+1].std().item(), error_bound, f"Test failed: std deviation {y[:, N_FRAMES:N_FRAMES + CUT_FRAME+1].std()} exceeded {error_bound} in second sequence before CUT_FRAME")
         # TODO: CHECK THIS INFORMATION BOTTLENECK
-        self.assertGreaterEqual(  y[:, N_FRAMES + CUT_FRAME+1:].std(dim=2).mean().item(), 0.1/np.sqrt(N_FRAMES*IMG_RESOLUTION**2), f"Test failed: post-CUT_FRAME frames in second sequence did not respond to perturbation")
+        self.assertGreaterEqual(  y[:, N_FRAMES + CUT_FRAME+1:].std(dim=2).mean().item(), 0.1/np.sqrt(N_FRAMES*img_resolution**2), f"Test failed: post-CUT_FRAME frames in second sequence did not respond to perturbation")
 
 
 class TestUNet(unittest.TestCase):
@@ -204,18 +212,7 @@ class TestUNet(unittest.TestCase):
         torch.cuda.manual_seed_all(SEED)
 
         # Initialize UNet and Precond
-        cls.unet = UNet(
-            img_resolution=IMG_RESOLUTION,
-            img_channels=IMG_CHANNELS,
-            label_dim=0,
-            model_channels=32,
-            channel_mult=[1, 2, 2, 4],
-            channel_mult_noise=None,
-            channel_mult_emb=None,
-            num_blocks=3,
-            video_attn_resolutions=[8],
-            frame_attn_resolutions=[16]
-        ).to("cuda").to(dtype)
+        cls.unet = UNet.from_pretrained('saved_models/unet_11.0M.pt').to("cuda").to(dtype)
         print(f"Number of UNet parameters: {sum(p.numel() for p in cls.unet.parameters()) // 1e6}M")
 
     ## from this point onward the tests are obsolete so they might 
@@ -224,12 +221,12 @@ class TestUNet(unittest.TestCase):
         self.unet.train()
         x = torch.randn(2, 16 * N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
         noise_level = torch.zeros(x.shape[:2], device="cuda", dtype=dtype)
-        y_train, _ = self.unet(x, noise_level, conditioning=None)
+        y_train, _, _ = self.unet(x, noise_level, conditioning=None)
 
         self.unet.eval()
         x_eval = torch.cat((x[:, :CUT_FRAME], x[:, CUT_FRAME + N_FRAMES].unsqueeze(1)), dim=1)
         noise_level_eval = torch.cat((noise_level[:, :CUT_FRAME], noise_level[:, CUT_FRAME + N_FRAMES].unsqueeze(1)), dim=1)
-        y_eval, _ = self.unet(x_eval, noise_level_eval, conditioning=None) # if i dont pass explicitly the last argument cache={} it doesn't work despite the fact that it is the default argument!
+        y_eval, _, _ = self.unet(x_eval, noise_level_eval, conditioning=None) # if i dont pass explicitly the last argument cache={} it doesn't work despite the fact that it is the default argument!
         
         std_diff_1 = (y_train[:, :CUT_FRAME] - y_eval[:, :-1]).std().item()
         std_diff_2 = (y_train[:, CUT_FRAME + N_FRAMES] - y_eval[:, -1]).std().item()
@@ -264,11 +261,14 @@ class TestMPCausal3DGatedConv(unittest.TestCase):
     def setUpClass(cls):
         torch.manual_seed(SEED)
         torch.cuda.manual_seed_all(SEED)
-        cls.conv3d = MPCausal3DGatedConv(IMG_CHANNELS, IMG_CHANNELS, kernel=(3,3,3)).to("cuda").to(dtype)
+
+        cls.unet = UNet.from_pretrained('saved_models/unet_11.0M.pt').to("cuda").to(dtype)
+        cls.conv3d = cls.unet.enc['32x32_block0'].conv_res0
     
     def test_conv_consistency_between_train_and_eval(self):
         self.conv3d.train()
-        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_channels=self.conv3d.weight.weight.shape[1]
+        x = torch.randn(BATCH_SIZE * 2 * N_FRAMES,  img_channels, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
         c_noise = torch.randn(BATCH_SIZE, 2 * N_FRAMES, device =x.device)
         y_train, _ = self.conv3d(x, None, BATCH_SIZE, c_noise)
         
@@ -290,7 +290,8 @@ class TestMPCausal3DGatedConv(unittest.TestCase):
     
     def test_conv_consistency_between_cached_and_non_cached(self):
         self.conv3d.eval()
-        x = torch.randn(BATCH_SIZE, N_FRAMES, IMG_CHANNELS, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
+        img_channels=self.conv3d.weight.weight.shape[1]
+        x = torch.randn(BATCH_SIZE, N_FRAMES,  img_channels, IMG_RESOLUTION, IMG_RESOLUTION, device="cuda", dtype=dtype)
         c_noise = torch.randn(BATCH_SIZE, N_FRAMES, device = x.device)
         context, last_frame = x[:, :-1], x[:,-1:]
         x = einops.rearrange(x, 'b t ... -> (b t) ...')
@@ -313,7 +314,8 @@ class TestMPCausal3DGatedConv(unittest.TestCase):
         self.conv3d.eval()
         b = BATCH_SIZE
         img_res = 16
-        x = torch.randn(b, N_FRAMES, IMG_CHANNELS, img_res, img_res, device="cuda", dtype=dtype)
+        img_channels=self.conv3d.weight.weight.shape[1]
+        x = torch.randn(b, N_FRAMES, img_channels, img_res, img_res, device="cuda", dtype=dtype)
         c_noise = torch.randn(b, N_FRAMES, device = x.device)
         context, second_last, last_frame = x[:, :-2], x[:,-2:-1], x[:,-1:]
         
